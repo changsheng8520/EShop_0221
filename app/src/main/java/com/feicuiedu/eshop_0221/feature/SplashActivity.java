2d2f0f3aedd7bc3c361b57fc182e5ec4d4a931e1
gqq <gqq519@163.com>
2017-02-21 10:52:19 +0800

    前置资源导入

---
+++ app/src/main/java/com/feicuiedu/eshop_0221/feature/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.feicuiedu.eshop_0221.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.feicuiedu.eshop_0221.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
