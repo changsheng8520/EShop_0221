078cde7167aa65dbbef11d716908846ae475f0eb
gqq <gqq519@163.com>
2017-02-21 11:12:16 +0800

    git使用的测试提交

--- app/src/main/java/com/feicuiedu/eshop_0221/feature/SplashActivity.java
+++ app/src/main/java/com/feicuiedu/eshop_0221/feature/SplashActivity.java
@@ -11,5 +11,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+        
     }
 }
