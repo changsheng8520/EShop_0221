83b5f28e5739b18785b1be11650331f3ffa422c2
gqq <gqq519@163.com>
2017-02-21 17:38:02 +0800

    Splash：页面搭建及实现

---
+++ app/src/main/java/com/feicuiedu/eshop_0221/feature/EShopMainActivity.java
@@ -0,0 +1,15 @@
+package com.feicuiedu.eshop_0221.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.feicuiedu.eshop_0221.R;
+
+public class EShopMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eshop_main);
+    }
+}
